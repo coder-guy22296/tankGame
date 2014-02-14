bf8cff6d6171bbc4e71dde11a5bd6df2692c0f2f
coder-guy22296 <canpy30@gmail.com>
2014-02-13 22:05:23 -0800

    update Can

--- TankGame/src/tank/Tank.java
+++ TankGame/src/tank/Tank.java
@@ -1,14 +1,19 @@
 package tank;
 
 public class Tank {
-	public void getTankLocation(){
-		
-	}
+	private int tankRotation = 0;
+	private int xLocation = 0;
+	private int yLocation = 0;
+	
 	public void setTankLocation(int x, int y){
-		
+		xLocation = x;
+		yLocation = y;
 	}
 	public void setTankRotation(int degrees){
 		
 	}
-	
+	public int getTankRotation(){
+		return tankRotation;
+	}
+
 }
