32acc0db185496f1a924b55882108f321442fb8c
rbrott <r.brott@comcast.net>
2014-02-13 17:16:17 -0800

    Wrote the main method of the Main class. Added basic functionality to the Board class. Ryan

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/6a/c03782de1395001311749370a74ec6fb
@@ -0,0 +1,9 @@
+package tank;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
--- TankGame/src/tank/Main.java
+++ TankGame/src/tank/Main.java
@@ -1,9 +1,26 @@
 package tank;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+	
+	Main() {
+		Board b = new Board();
+		add(b);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setSize(500, 500);
+		setTitle("Tank Game");
+		setVisible(true);
+	}
 	
 	public static void main(String[] args) {
-		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Main m = new Main();
+			}
+		});
 	}
 	
 }
