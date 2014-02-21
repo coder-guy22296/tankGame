6c2f4a72ab535de316426072f73836289c59359a
coder-guy22296 <canpy30@gmail.com>
2014-02-21 15:17:31 -0800

    added damage mechanic to laser --Can Yildirim

--- TankGame/src/tank/Laser.java
+++ TankGame/src/tank/Laser.java
@@ -1,16 +1,25 @@
 package tank;
 
 public class Laser extends Weapon {
-	private int beamIntensity = 0;//percent 0-100
+	
 	private int damageDealt = 0;
+	private int storedEnergy = 0;// 1k max
+	private int beamIntensity = 0;//percent 0-100
+	private double damagePerPercent = 2;
+	private double totalDammage = 0;// damagePerPercent*beamIntensity
+	
 	@Override
 	void reload() {
 		// TODO Auto-generated method stub
+		
+		storedEnergy = 1000;
 		beamIntensity = 100;
 	}
 	@Override
 	void fire() {
-		// TODO Auto-generated method stub
+		totalDammage = damagePerPercent*beamIntensity;
+		storedEnergy -= 125;
+		beamIntensity = storedEnergy/10;
 		
 	}
 	@Override
